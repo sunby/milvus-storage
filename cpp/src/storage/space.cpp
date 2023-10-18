f78ed1d9cbcfbd9ee159b859de0a42eb1909943c
sunby <sunbingyi1992@gmail.com>
2023-10-18 10:48:08 +0800

    aa

--- cpp/src/storage/space.cpp
+++ cpp/src/storage/space.cpp
@@ -237,6 +237,7 @@ Result<std::unique_ptr<Space>> Space::Open(const std::string& uri, Options optio
   RETURN_ARROW_NOT_OK(uri_parser.Parse(uri));
   path = uri_parser.path();
 
+  LOG_STORAGE_DEBUG_ << "Open space: " << path;
   RETURN_ARROW_NOT_OK(fs->CreateDir(GetManifestDir(path)));
   RETURN_ARROW_NOT_OK(fs->CreateDir(GetScalarDataDir(path)));
   RETURN_ARROW_NOT_OK(fs->CreateDir(GetVectorDataDir(path)));
